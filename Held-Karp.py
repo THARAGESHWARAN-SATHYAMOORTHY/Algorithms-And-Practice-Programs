7b0df077d62d060b03dfc9c8ea3d7dd92154e44c
Tharageshwaran-S <s.tharageshwaran6343000@gmail.Com>
2024-01-06 23:21:49 +0530

    Create Held-Karp.py

---
+++ Held-Karp.py
@@ -0,0 +1,47 @@
+import itertools
+import random
+import sys
+import json
+
+def held_karp(dists):
+    n = len(dists)
+    C = {}
+    for k in range(1, n):
+        C[(1 << k, k)] = (dists[0][k], 0)
+    for subset_size in range(2, n):
+        for subset in itertools.combinations(range(1, n), subset_size):
+            bits = 0
+            for bit in subset:
+                bits |= 1 << bit
+            for k in subset:
+                prev = bits & ~(1 << k)
+
+                res = []
+                for m in subset:
+                    if m == 0 or m == k:
+                        continue
+                    res.append((C[(prev, m)][0] + dists[m][k], m))
+                C[(bits, k)] = min(res)
+    bits = (2**n - 1) - 1
+
+    res = []
+    for k in range(1, n):
+        res.append((C[(bits, k)][0] + dists[k][0], k))
+    opt, parent = min(res)
+  
+    path = []
+    for i in range(n - 1):
+        path.append(parent)
+        new_bits = bits & ~(1 << parent)
+        _, parent = C[(bits, parent)]
+        bits = new_bits
+    path.append(0)
+    return opt, list(reversed(path))
+
+file = open("input.json")
+data = json.load(file)
+
+for row in data:
+    print(''.join([str(n).rjust(3, ' ') for n in row]))
+print('')
+print(held_karp(data))
