b4e8888f91aa278a379d41fcb1af6a3e28aee738
Tharageshwaran-S <s.tharageshwaran6343000@gmail.Com>
2024-01-07 20:18:34 +0530

    Update Held-Karp.py

--- Held-Karp.py
+++ Held-Karp.py
@@ -1,18 +1,38 @@
 import itertools
-import random
-import sys
-import json
+
 
 def held_karp(dists):
+    """
+    Implementation of Held-Karp, an algorithm that solves the Traveling
+    Salesman Problem using dynamic programming with memoization.
+
+    Parameters:
+        dists: distance matrix
+
+    Returns:
+        A tuple, (cost, path).
+    """
     n = len(dists)
+
+    # Maps each subset of the nodes to the cost to reach that subset, as well
+    # as what node it passed before reaching this subset.
+    # Node subsets are represented as set bits.
     C = {}
+
+    # Set transition cost from initial state
     for k in range(1, n):
         C[(1 << k, k)] = (dists[0][k], 0)
+
+    # Iterate subsets of increasing length and store intermediate results
+    # in classic dynamic programming manner
     for subset_size in range(2, n):
         for subset in itertools.combinations(range(1, n), subset_size):
+            # Set bits for all nodes in this subset
             bits = 0
             for bit in subset:
                 bits |= 1 << bit
+
+            # Find the lowest cost to get to this subset
             for k in subset:
                 prev = bits & ~(1 << k)
 
@@ -22,26 +42,35 @@ def held_karp(dists):
                         continue
                     res.append((C[(prev, m)][0] + dists[m][k], m))
                 C[(bits, k)] = min(res)
+
+    # We're interested in all bits but the least significant (the start state)
     bits = (2**n - 1) - 1
 
+    # Calculate optimal cost
     res = []
     for k in range(1, n):
         res.append((C[(bits, k)][0] + dists[k][0], k))
     opt, parent = min(res)
-  
+
+    # Backtrack to find full path
     path = []
     for i in range(n - 1):
         path.append(parent)
         new_bits = bits & ~(1 << parent)
         _, parent = C[(bits, parent)]
         bits = new_bits
+
+    # Add implicit start state
     path.append(0)
+
     return opt, list(reversed(path))
 
-#file = open("input.json")
-#data = json.load(file)
 
-for row in data:
-    print(''.join([str(n).rjust(3, ' ') for n in row]))
-print('')
-print(held_karp(data))
+f = [[0, 2, 9, 10], [1, 0, 6, 4], [15, 7, 0, 8], [6, 3, 12, 0]]
+
+for row in f:
+    print("".join([str(n).rjust(3, " ") for n in row]))
+
+print("")
+
+print(held_karp(f))
