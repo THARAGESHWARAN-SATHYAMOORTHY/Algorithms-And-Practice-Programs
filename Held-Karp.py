f9edfc8ff8e4670ef4d70e9c0445443624386d2d
Tharageshwaran-S <s.tharageshwaran6343000@gmail.Com>
2024-01-17 18:57:39 +0530

    Update Held-Karp.py

--- Held-Karp.py
+++ Held-Karp.py
@@ -1,38 +1,19 @@
 import itertools
 
-
 def held_karp(dists):
-    """
-    Implementation of Held-Karp, an algorithm that solves the Traveling
-    Salesman Problem using dynamic programming with memoization.
-
-    Parameters:
-        dists: distance matrix
-
-    Returns:
-        A tuple, (cost, path).
-    """
+    
     n = len(dists)
-
-    # Maps each subset of the nodes to the cost to reach that subset, as well
-    # as what node it passed before reaching this subset.
-    # Node subsets are represented as set bits.
     C = {}
-
-    # Set transition cost from initial state
+    
     for k in range(1, n):
         C[(1 << k, k)] = (dists[0][k], 0)
 
-    # Iterate subsets of increasing length and store intermediate results
-    # in classic dynamic programming manner
     for subset_size in range(2, n):
         for subset in itertools.combinations(range(1, n), subset_size):
-            # Set bits for all nodes in this subset
             bits = 0
             for bit in subset:
                 bits |= 1 << bit
 
-            # Find the lowest cost to get to this subset
             for k in subset:
                 prev = bits & ~(1 << k)
 
@@ -43,16 +24,13 @@ def held_karp(dists):
                     res.append((C[(prev, m)][0] + dists[m][k], m))
                 C[(bits, k)] = min(res)
 
-    # We're interested in all bits but the least significant (the start state)
     bits = (2**n - 1) - 1
 
-    # Calculate optimal cost
     res = []
     for k in range(1, n):
         res.append((C[(bits, k)][0] + dists[k][0], k))
     opt, parent = min(res)
 
-    # Backtrack to find full path
     path = []
     for i in range(n - 1):
         path.append(parent)
@@ -60,7 +38,6 @@ def held_karp(dists):
         _, parent = C[(bits, parent)]
         bits = new_bits
 
-    # Add implicit start state
     path.append(0)
 
     return opt, list(reversed(path))
@@ -74,3 +51,8 @@ for row in f:
 print("")
 
 print(held_karp(f))
+
+'''
+Time Complexity  : O(n^2 * 2^n)
+Space Complexity : O(n * 2^n)
+'''
